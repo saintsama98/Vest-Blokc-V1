92a3b5ba7c2bd226cafb8f309ef8504be4f1829a
Aditya Biradar <biradaraditya9712@gmail.com>
2025-08-25 19:51:36 +0530

    Registry updated

--- src/registry/VestingRegistry.sol
+++ src/registry/VestingRegistry.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.20;
 
 import "./IVestingRegistry.sol";
+import "./VestingRegistryBase.sol";
 
-abstract contract VestingRegistryBase is IVestingRegistry {
-    mapping(address => address[]) internal _userVestings;
-    address[] internal _allVestings;
+abstract contract VestingRegistry is IVestingRegistry, VestingRegistryBase {
 
     function getUserVestings(address user) public view override returns (address[] memory) {
         return _userVestings[user];
